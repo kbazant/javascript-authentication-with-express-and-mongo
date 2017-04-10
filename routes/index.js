4e700c21d6c24f96cab9c76c51bd7cfac75e8037
kbazant <krizz100@yahoo.com>
2017-04-10 23:31:42 +0100

    added get route to display registration form

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+//GET /register
+router.get('./register', function (req, res, next) {
+	return res.send('Register today!');
+});
+
 // GET /
-router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+router.get('/', function (req, res, next) {
+	return res.render('index', {
+		title: 'Home'
+	});
 });
 
 // GET /about
-router.get('/about', function(req, res, next) {
-  return res.render('about', { title: 'About' });
+router.get('/about', function (req, res, next) {
+	return res.render('about', {
+		title: 'About'
+	});
 });
 
 // GET /contact
-router.get('/contact', function(req, res, next) {
-  return res.render('contact', { title: 'Contact' });
+router.get('/contact', function (req, res, next) {
+	return res.render('contact', {
+		title: 'Contact'
+	});
 });
 
 module.exports = router;
