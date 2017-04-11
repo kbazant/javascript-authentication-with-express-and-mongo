d75cee0c6a835942e22934792f4ff2d834269a99
kbazant <krizz100@yahoo.com>
2017-04-12 00:38:10 +0100

    added 'mongoose' variable

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 
 // parse incoming requests
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 
 // serve static files from /public
 app.use(express.static(__dirname + '/public'));
@@ -18,23 +21,23 @@ var routes = require('./routes/index');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('File Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('File Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 // define as the last app.use callback
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 // listen on port 3000
 app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
+	console.log('Express app listening on port 3000');
 });
