8c7f19767536a72456c1200ea473b15ce8ab35f0
kbazant <krizz100@yahoo.com>
2017-04-13 02:11:22 +0100

    added objects for email, name, favoriteBook and password

---
+++ models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true
+	},
+	name: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	favoriteBook: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	password {
+		type: String,
+		required: true
+	}
+})
