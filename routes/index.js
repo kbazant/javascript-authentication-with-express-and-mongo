16797edaf9cd99202b04d2d31e297dd0d5da8e53
kbazant <krizz100@yahoo.com>
2017-04-17 19:39:11 +0100

    installed crypt

--- routes/index.js
+++ routes/index.js
@@ -4,62 +4,68 @@ var User = require('../models/user');
 
 //GET /register
 router.get('/register', function (req, res, next) {
-	return res.render('register', {
-		title: 'Sign Up'
-	});
+			return res.render('register', {
+				title: 'Sign Up'
+			});
+
+			//POST /register
+			router.post('/register', function (req, res, next) {
+				if (req.body.email &&
+					req.body.name &&
+					req.body.favoriteBook &&
+					req.body.password &&
+					req.body.confirmPassword) {
+
+					// confirm that user typed same password twice
+					if (req.body.password !== req.body.confirmPassword) {
+						var err = new Error('Passwords do not match.');
+						error.status = 400;
+						return next(err);
+					}
+					//create object with form input
+					var userData = {
+						emai: req.body.email,
+						name: req.body.name,
+						favoriteBook: req.body.favoriteBook,
+						password: req.body.password
+					};
+
+					//use schema's 'create' method to insert document into Mongo
+					User.create(userData, function (error, user) {
+						if (error) {
+							return next(error);
+						} else {
+							return res.redirect('/profile');
+						}
+					});
+
+				} else {
+					var err = new Error('All fields required.');
+					err.status = 400;
+					return next(err);
+				}
+			});
 
 
-//POST /register
-router.post('/register', function (req, res, next) {
-	if (req.body.email &&
-		req.body.name &&
-		req.body.favoriteBook &&
-		req.body.password &&
-		req.body.confirmPassword) {
+			// GET /
+			router.get('/', function (req, res, next) {
+				return res.render('index', {
+					title: 'Home'
+				});
+			});
 
-		// confirm that user typed same password twice
-		if (req.body.password !== req.body.confirmPassword) {
-			var err = new Error('Passwords do not match.');
-			error.status = 400;
-			return next(err);
-		}
-		
-		//create object with form input
-		var userData = {
-			emai: req.body.email,
-			name: req.body.name,
-			favoriteBook: req.body.favoriteBook,
-			password: req.body.password
-		};
-		
-	} else {
-		var err = new Error('All fields required.');
-		err.status = 400;
-		return next(err);
-	}
-}); 
+			// GET /about
+			router.get('/about', function (req, res, next) {
+				return res.render('about', {
+					title: 'About'
+				});
+			});
 
+			// GET /contact
+			router.get('/contact', function (req, res, next) {
+				return res.render('contact', {
+					title: 'Contact'
+				});
+			});
 
-// GET /
-router.get('/', function (req, res, next) {
-	return res.render('index', {
-		title: 'Home'
-	});
-});
-
-// GET /about
-router.get('/about', function (req, res, next) {
-	return res.render('about', {
-		title: 'About'
-	});
-});
-
-// GET /contact
-router.get('/contact', function (req, res, next) {
-	return res.render('contact', {
-		title: 'Contact'
-	});
-});
-
-module.exports = router;
-	 
+			module.exports = router;
